f64ef8d5782745198aa8c91162d5bfb3aa6f14b6
hzw649 <995904436@qq.com>
2020-07-13 16:44:35 +0800

    add DeleteServlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -0,0 +1,29 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+@WebServlet("/delete")
+public class DeleteServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        long id = Long.parseLong(request.getParameter("id"));
+
+        Key taskEntityKey = KeyFactory.createKey("Comments", id);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.delete(taskEntityKey);
+    }
+
+}
