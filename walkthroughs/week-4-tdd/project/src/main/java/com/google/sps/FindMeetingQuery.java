f946ff68a30a9537495271d37f6ab89dfa04334f
hzw649 <995904436@qq.com>
2020-07-24 14:41:12 +0800

    finish FindMeetingQuery

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,32 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        Collection<TimeRange> result = new ArrayList<>();
+        int start = -1;
+        int end = -1;
+        while (start<1440)
+        {
+            int new_start = end+1;
+            int new_end = TimeRange.END_OF_DAY+1;
+            for (Event event : events)
+                if (!Collections.disjoint(event.getAttendees(), request.getAttendees()))
+                {
+                    TimeRange timeRange = event.getWhen();
+                    if (timeRange.contains(new_start))
+                        new_start = timeRange.end();
+                    if (timeRange.start() > end && timeRange.start() < new_end)
+                        new_end = timeRange.start();
+                }
+            if (new_end - new_start >= request.getDuration())
+                result.add(TimeRange.fromStartEnd(new_start, new_end, false));
+            start = new_start;
+            end = new_end;
+        }
+        return result;
+    }
 }
